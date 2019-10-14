10ad826410414050f00585b2e6a85de2d4e8dc80
DaeFennek <DaeFennek@gmail.com>
2019-10-14 21:28:38 +0200

    basic matrix4x4 methods

--- src/utils/matrix4x4.cpp
+++ src/utils/matrix4x4.cpp
@@ -1,73 +1,80 @@
 #include <sstream>
+#include <cstring>
 #include "utils/matrix4x4.h"
+
+Matrix4x4::Matrix4x4()
+{
+    std::memset(matrix, 0, sizeof(matrix));
+}
 
 Matrix4x4::Matrix4x4(const Mtx& mtx)
 {
-	_11 = mtx[0][0];
-	_12 = mtx[0][1];
-	_13 = mtx[0][2];
-	_14 = mtx[0][3];
+    matrix[0][0] = mtx[0][0];
+    matrix[0][1] = mtx[0][1];
+    matrix[0][2] = mtx[0][2];
+    matrix[0][3] = mtx[0][3];
 
-	_21 = mtx[1][0];
-	_22 = mtx[1][1];
-	_23 = mtx[1][2];
-	_24 = mtx[1][3];
+    matrix[1][0] = mtx[1][0];
+    matrix[1][1] = mtx[1][1];
+    matrix[1][2] = mtx[1][2];
+    matrix[1][3] = mtx[1][3];
 
-	_31 = mtx[2][0];
-	_32 = mtx[2][1];
-	_33 = mtx[2][2];
-	_34 = mtx[2][3];
+    matrix[2][0] = mtx[2][0];
+    matrix[2][1] = mtx[2][1];
+    matrix[2][2] = mtx[2][2];
+    matrix[2][3] = mtx[2][3];
 
-	_41 = 0.0f;
-	_42 = 0.0f;
-	_43 = 0.0f;
-	_44 = 0.0f;
+    matrix[3][0] = 0.0f;
+    matrix[3][1] = 0.0f;
+    matrix[3][2] = 0.0f;
+    matrix[3][3] = 0.0f;
 }
 
 Matrix4x4::Matrix4x4(const Mtx44& mtx)
 {
-	_11 = mtx[0][0];
-	_12 = mtx[0][1];
-	_13 = mtx[0][2];
-	_14 = mtx[0][3];
+    matrix[0][0] = mtx[0][0];
+    matrix[0][1] = mtx[0][1];
+    matrix[0][2] = mtx[0][2];
+    matrix[0][3] = mtx[0][3];
 
-	_21 = mtx[1][0];
-	_22 = mtx[1][1];
-	_23 = mtx[1][2];
-	_24 = mtx[1][3];
+    matrix[1][0] = mtx[1][0];
+    matrix[1][1] = mtx[1][1];
+    matrix[1][2] = mtx[1][2];
+    matrix[1][3] = mtx[1][3];
 
-	_31 = mtx[2][0];
-	_32 = mtx[2][1];
-	_33 = mtx[2][2];
-	_34 = mtx[2][3];
+    matrix[2][0] = mtx[2][0];
+    matrix[2][1] = mtx[2][1];
+    matrix[2][2] = mtx[2][2];
+    matrix[2][3] = mtx[2][3];
 
-	_41 = mtx[3][0];
-	_42 = mtx[3][1];
-	_43 = mtx[3][2];
-	_44 = mtx[3][3];
+    matrix[3][0] = mtx[3][0];
+    matrix[3][1] = mtx[3][1];
+    matrix[3][2] = mtx[3][2];
+    matrix[3][3] = mtx[3][3];
 }
+
 
 std::string Matrix4x4::ToString() const
 {
 	std::stringstream ss;
-	ss << "11: " << _11;
-	ss << " 12: " << _12;
-	ss << " 13: " << _13;
-	ss << " 14: " << _14 << std::endl;
+    ss << matrix[0][0] << " "
+    << matrix[0][1] << " "
+    << matrix[0][2] << " "
+    << matrix[0][3] << std::endl;
 
-	ss << "21: " << _21;
-	ss << " 22: " << _22;
-	ss << " 23: " << _23;
-	ss << " 24: " << _24 << std::endl;
+    ss << matrix[1][0] << " "
+    << matrix[1][1] << " "
+    << matrix[1][2] << " "
+    << matrix[1][3] << std::endl;
 
-	ss << "31: " << _31;
-	ss << " 32: " << _32;
-	ss << " 33: " << _33;
-	ss << " 34: " << _34 << std::endl;
+    ss << matrix[2][0] << " "
+    << matrix[2][1] << " "
+    << matrix[2][2] << " "
+    << matrix[2][3] << std::endl;
 
-	ss << "41: " << _41;
-	ss << " 42: " << _42;
-	ss << " 43: " << _43;
-	ss << " 44: " << _44 << std::endl;
+    ss << matrix[3][0] << " "
+    << matrix[3][1] << " "
+    << matrix[3][2] << " "
+    << matrix[3][3] << std::endl;
 	return ss.str();
 }
--- src/utils/matrix4x4.h
+++ src/utils/matrix4x4.h
@@ -8,19 +8,106 @@
 class Matrix4x4
 {
 public:
+    Matrix4x4();
 	Matrix4x4(const Mtx& mtx);
 	Matrix4x4(const Mtx44& mtx);
 
-	// Implement later!
-	Matrix4x4(const Matrix4x4&) = delete;
-	Matrix4x4(Matrix4x4&&) = delete;
-	void operator=(const Matrix4x4&) = delete;
-	void operator=(Matrix4x4&&) = delete;
+    Matrix4x4(const Matrix4x4&) = default;
+    Matrix4x4(Matrix4x4&&) = default;
+    Matrix4x4& operator=(const Matrix4x4&) = default;
+    Matrix4x4& operator=(Matrix4x4&&) = default;
 
-	std::string ToString() const;
+    inline void SetIdentity();
+	std::string ToString() const;    
 
-	float _11, _12, _13, _14;
-	float _21, _22, _23, _24;
-	float _31, _32, _33, _34;
-	float _41, _42, _43, _44;
+    inline Matrix4x4 operator+(const Matrix4x4& other) const;
+    inline Matrix4x4 operator*(const Matrix4x4& other) const;
+    inline Matrix4x4 operator*(float value) const;
+    inline void operator+=(const Matrix4x4& other);
+    inline void operator*=(const Matrix4x4& other);
+    inline bool operator==(const Matrix4x4& other) const;
+    inline bool operator!=(const Matrix4x4& other) const;
+
+private:
+    float matrix[4][4];
 };
+
+inline void Matrix4x4::SetIdentity()
+{
+    matrix[0][0] = 1; matrix[0][1] = 0;  matrix[0][2] = 0;  matrix[0][3] = 0;
+    matrix[1][0] = 0; matrix[1][1] = 1;  matrix[1][2] = 0;  matrix[1][3] = 0;
+    matrix[2][0] = 0; matrix[2][1] = 0;  matrix[2][2] = 1;  matrix[2][3] = 0;
+    matrix[3][0] = 0; matrix[3][1] = 0;  matrix[3][2] = 0;  matrix[3][3] = 1;
+}
+
+inline Matrix4x4 Matrix4x4::operator+(const Matrix4x4& other) const
+{
+    Matrix4x4 result;
+    for (uint32_t x = 0; x < 4; ++x)
+    {
+        for (uint32_t y = 0; y < 4; ++y)
+        {
+            result.matrix[x][y] = matrix[x][y] + other.matrix[x][y];
+        }
+    }
+    return result;
+}
+
+inline Matrix4x4 Matrix4x4::operator*(const Matrix4x4& other) const
+{
+    Matrix4x4 result;
+    for(uint32_t i = 0; i < 4; ++i)
+    {
+        for(uint32_t j = 0; j < 4; ++j)
+        {
+            for(uint32_t k = 0; k < 4; ++k)
+            {
+                result.matrix[i][j] += matrix[i][k] * other.matrix[k][j];
+            }
+        }
+    }
+    return result;
+}
+
+inline Matrix4x4 Matrix4x4::operator*(float value) const
+{
+    Matrix4x4 result;
+    for (uint32_t x = 0; x < 4; ++x)
+    {
+        for (uint32_t y = 0; y < 4; ++y)
+        {
+            result.matrix[x][y] = matrix[x][y] * value;
+        }
+    }
+    return result;
+}
+
+inline void Matrix4x4::operator+=(const Matrix4x4& other)
+{
+    *this = *this + other;
+}
+
+inline void Matrix4x4::operator*=(const Matrix4x4& other)
+{
+    *this = *this * other;
+}
+
+inline bool Matrix4x4::operator==(const Matrix4x4& other) const
+{
+    for (uint32_t x = 0; x < 4; ++x)
+    {
+        for (uint32_t y = 0; y < 4; ++y)
+        {
+            if (matrix[x][y] != other.matrix[x][y]) // maybe add a equals method with a custom tolerance?
+            {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+inline bool Matrix4x4::operator!=(const Matrix4x4& other) const
+{
+    return !(*this == other);
+}
